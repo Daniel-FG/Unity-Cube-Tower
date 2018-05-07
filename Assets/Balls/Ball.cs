4139d8983ec5ad0423ecb552d8fc27d2d3d6e1a0
DANIEL-FG\o9708 <o97082@yahoo.com.tw>
2018-05-08 00:41:31 +0800

    Added basic structure of the game and Ball.cs

---
+++ Assets/Balls/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float fallingSpeed = 0.1f;
+    private Rigidbody ball;
+
+    private void Start()
+    {
+        ball = GetComponent<Rigidbody>();
+        InvokeRepeating("Drop", 0f, Time.deltaTime);
+    }
+    private void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 direction = collision.transform.position - transform.position;
+        if (Vector3.Dot(Vector3.down, direction) > 0)
+        {
+            print("Down");
+            CancelInvoke();
+        }
+        if (Vector3.Dot(Vector3.down, direction) < 0)
+        {
+            print("Up");
+        }
+        if (Vector3.Dot(Vector3.down, direction) == 0)
+        {
+            print("Side");
+        }
+        print(collision.gameObject.name);
+    }
+
+    private void Drop()
+    {
+        transform.Translate(Vector3.down * fallingSpeed);
+        print("falling...");
+    }
+}
