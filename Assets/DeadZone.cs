6d17a81e14f10fa45b6e9aea92c2c533de60a5eb
DANIEL-FG\o9708 <o97082@yahoo.com.tw>
2018-05-08 23:40:47 +0800

    Added dead zone

---
+++ Assets/DeadZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    private GameManager gameManager;
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
